9f4df33129672982049fd687c63993ed77975f77
Lauri Gustafsson <me@gustafla.space>
2020-03-12 22:07:21 +0200

    Add SQLAlchemy init code

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from application.views import views
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///reports.db"
+app.config["SQLALCHEMY_ECHO"] = True
 app.register_blueprint(views)
+
+db = SQLAlchemy(app)
+
+db.create_all()
